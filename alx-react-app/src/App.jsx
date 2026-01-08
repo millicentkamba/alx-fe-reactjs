a2857810290aa7f197e3a2fe74a4ad84efb1f3b8
Millie Kamba <millicentkamba394@gmail.com>
2026-01-08 09:38:56 +0200

    Add UserProfile component with props and render in App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,17 +3,27 @@
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 import './App.css'
 
 function App() {
   return (
     <div>
-      {/* Render the components in the required order */}
+      {/* Render components in the required order */}
       <Header />
       <MainContent />
+
+      {/* Render UserProfile component with props */}
+      <UserProfile 
+        name="Alice" 
+        age={25} 
+        bio="Loves hiking and photography" 
+      />
+
       <Footer />
     </div>
   )
 }
 
 export default App
+
