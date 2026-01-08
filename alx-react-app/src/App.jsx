1a160b37c2cb21c3f5d5c098689f893104079527
Millie Kamba <millicentkamba394@gmail.com>
2026-01-08 09:33:22 +0200

    Add Header, MainContent, Footer components and render them in App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,32 +1,17 @@
-import { useState } from 'react'
-import WelcomeMessage from './components/WelcomeMessage'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// alx-react-app/src/App.jsx
+
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      <WelcomeMessage />
-
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <button onClick={() => setCount(count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      {/* Render the components in the required order */}
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   )
 }
